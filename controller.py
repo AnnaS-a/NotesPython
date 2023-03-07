d548cdfb47401f387331cb1e36358ebb958a32d8
anna <slukin1@yandex.ru>
2023-03-07 17:35:50 +0700

    Controller

--- controller.py
+++ controller.py
@@ -7,3 +7,26 @@ class Controller(object):
     def create_note(self, note):
         self.model.create_note(note)
         self.view.display_note_stored()
+
+    def notes_exist(self):
+        notes = self.model.read_notes()
+        if len(notes) == 0:
+            self.view.show_empty_list_message()
+            return False
+        else:
+            return True
+    
+    def note_id_exist(self, search_id):
+        notes = self.model.read_notes()
+        for note in notes:
+            if note.note_id == search_id:
+                return True
+        else:
+            self.view.display_note_id_not_exist(search_id)
+            return False
+        
+    def update_note(self, note_id, note):
+        self.model.update_note(note_id, note)
+        self.view.display_note_updated(note_id)
+
+    

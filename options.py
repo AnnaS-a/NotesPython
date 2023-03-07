a83b82fa096c9a8b80ba2bd217c1c84bc03917a4
anna <slukin1@yandex.ru>
2023-03-07 17:35:24 +0700

    Опции

--- options.py
+++ options.py
@@ -1,24 +1,51 @@
 import datetime
 from colorama import Fore, Style
 
+from controller import Controller
+from modelJson import ModelJson
+from view import View
 from note import Note
 
 def run():
     c = Controller(ModelJson("notes.json"), View())
 
     while True:
-        command = input(Fore.GREEN +
+        command = input(Fore.YELLOW +
                         'создать заметку - 1\n'
                         'редактировать заметку - 2\n'
                         'прочитать заметку - 3\n'
                         'удалить заметку - 4\n'
-                        'удалить все заметки - 5\n'
-                        'прочитать все заметки - 6\n'
+                        'прочитать все заметки - 5\n'
+                        'удалить все заметки - 6\n'
                         'выход - 7\n' +
                         'Выберете опцию: '
                         + Style.RESET_ALL)
         if command == '7':
             break
 
+        if command == '1':
+            print(Fore.GREEN + '\nСоздать заметку:' + Style.RESET_ALL)
+            c.create_note(get_note_data())
+
+        elif command == '2':
+            if c.notes_exist():
+                print(Fore.GREEN + '\nРедактировать заметку:' + Style.RESET_ALL)
+                updated_id = int(get_number())
+                if c.note_id_exist(updated_id):
+                    c.update_note(updated_id, get_note_data())
+
         
-            
+def get_note_data():
+    note_id = 0
+    date = datetime.datetime.now()
+    title = input('Введите заголовок заметки: ')
+    text = input('Введите тело заметки: ')
+    return Note(note_id, date, title, text)
+
+def get_number():
+    while True:
+        get_choice = input('Введите id заметки: ')
+        if get_choice.isdigit() and int(get_choice) > 0:
+            return get_choice
+        else:
+            print(Fore.RED + 'Введите целое положительное число!' + Style.RESET_ALL)

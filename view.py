398c825e1a2457c3a32a42f2e8ffb5aa43dd6ff5
anna <slukin1@yandex.ru>
2023-03-07 18:45:32 +0700

    View

--- view.py
+++ view.py
@@ -26,3 +26,22 @@ class View(object):
         print(Fore.YELLOW + '-------------------------------------' + Style.RESET_ALL)
         print(Fore.GREEN + 'Заметка с id:{} обновлена успешно!'.format(note_id) + Style.RESET_ALL)
         print(Fore.YELLOW + '-------------------------------------' + Style.RESET_ALL)
+
+    @staticmethod
+    def show_note(note):
+        print(Fore.YELLOW + '------------------------------------' + Style.RESET_ALL)
+        print(note)
+        print(Fore.YELLOW + '------------------------------------' + Style.RESET_ALL)
+
+    @staticmethod
+    def display_note_deletion(note_id):
+        print('--------------------------------------------------')
+        print(Fore.LIGHTRED_EX + 'Удаление заметки с id: {} выполнено!'.format(note_id) + Style.RESET_ALL)
+        print('--------------------------------------------------')
+
+    def display_note_id_not_exist(search_id):
+        return search_id
+    
+    
+
+
